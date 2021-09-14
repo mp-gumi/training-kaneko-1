4d842c0e1b41f37b621485e23b1274ea2fcd8165
mp-gumi <basil725woodstock@gmail.com>
2021-09-14 15:46:19 +0900

    fix: delete "returm"

--- src/components/NewsTop/NewsTitle/index.tsx
+++ src/components/NewsTop/NewsTitle/index.tsx
@@ -2,24 +2,22 @@
 import React from "react";
 import { css } from "@emotion/react";
 
-const NewsTitle = () => {
-  return (
-    <h2
-      css={css`
-        color: #333;
-        font-size: 34px;
-        font-weight: 600;
-        letter-spacing: 1px;
-        text-align: center;
-        @media (max-width: 767px) {
-          font-size: 7.8125vw;
-          padding: 26.5625vw 0 12.5vw 0;
-        }
-      `}
-    >
-      ニュース
-    </h2>
-  );
-};
+const NewsTitle = () => (
+  <h2
+    css={css`
+      color: #333;
+      font-size: 34px;
+      font-weight: 600;
+      letter-spacing: 1px;
+      text-align: center;
+      @media (max-width: 767px) {
+        font-size: 7.8125vw;
+        padding: 26.5625vw 0 12.5vw;
+      }
+    `}
+  >
+    ニュース
+  </h2>
+);
 
 export default NewsTitle;
